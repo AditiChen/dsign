98d8170700b0b7b34b2c3f97374014be8e1fef4e
Aliyah Chen <aliyah.chen199079@gmail.com>
2022-10-30 16:37:58 +0800

    Install firebase

---
+++ src/context/firebaseSDK.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy8mVX39MWAn628w2yd1bSv7vtnYBbXQQ",
+  authDomain: "d-sig-2f338.firebaseapp.com",
+  projectId: "d-sig-2f338",
+  storageBucket: "d-sig-2f338.appspot.com",
+  messagingSenderId: "95611418613",
+  appId: "1:95611418613:web:c852fad3d086567152d2bc",
+  measurementId: "G-WLG38432KP",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
