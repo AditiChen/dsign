b16c998aa0990798e80a759ba28b4f1a018a2454
Aliyah Chen <aliyah.chen199079@gmail.com>
2022-11-05 17:09:58 +0800

    Separate templates from edit version to view version

---
+++ src/components/singleProjectPageTemplates/Template0.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { t } from "i18next";
+
+interface Prop {
+  border?: string;
+  url?: string;
+  backgroundColor?: string;
+  top?: string;
+  left?: string;
+}
+
+interface InsertProp {
+  key: string;
+  photoUrl: string[];
+  content: string[];
+}
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  width: 1200px;
+  height: 760px;
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+const BackgroundImg = styled.div`
+  width: 1200px;
+  height: 760px;
+  background-image: ${(props: Prop) => props.url};
+  background-size: cover;
+  background-position: center;
+  opacity: 0.7;
+`;
+
+const MiddleContainer = styled.div`
+  padding: 100px 80px 50px 30px;
+  width: 450px;
+  height: 100%;
+  position: absolute;
+  left: 300px;
+  background-color: #616161;
+  opacity: 0.9;
+  box-shadow: 1px 0 3px #616161, -1px 0 3px #616161;
+`;
+
+const Context = styled.textarea`
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  color: #ffffff;
+  font-size: 24px;
+  line-height: 30px;
+  background-color: transparent;
+  border: none;
+  &::placeholder {
+    color: #b4b4b4;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const RightImg = styled.div`
+  width: 450px;
+  height: 645px;
+  background-image: ${(props: Prop) => props.url};
+  background-size: cover;
+  background-position: center;
+  position: absolute;
+  right: 50px;
+  z-index: 1;
+  box-shadow: 0 0 5px #3c3c3c;
+`;
+
+function Template0(props: InsertProp) {
+  const { photoUrl, content } = props;
+
+  return (
+    <Wrapper>
+      <BackgroundImg url={`url(${photoUrl[0]})`} />
+      <MiddleContainer>
+        <Context value={content} disabled />
+      </MiddleContainer>
+      <RightImg url={`url(${photoUrl[1]})`} />
+    </Wrapper>
+  );
+}
+
+export default Template0;
---
+++ src/components/singleProjectPageTemplates/TemplatesArr.tsx
@@ -0,0 +1,22 @@
+import Template0 from "./Template0";
+import Template1 from "./Template1";
+// import Template2 from "./Template2";
+// import Template3 from "./Template3";
+// import Template4 from "./Template4";
+// import Template5 from "./Template5";
+// import Template6 from "./Template6";
+// import Template7 from "./Template7";
+import GoogleMapInsert from "./GoogleMapAPI";
+
+const templatesArr = [
+  Template0,
+  Template1,
+  // Template2,
+  // Template3,
+  // Template4,
+  // Template5,
+  // Template6,
+  // Template7,
+  GoogleMapInsert,
+];
+export default templatesArr;
