40537b848d45e5c7864b66307321a131f4917d2b
Aliyah Chen <aliyah.chen199079@gmail.com>
2022-11-14 10:16:37 +0800

    Fix upload main url no loading status

--- src/pages/CreateNewProject/CreateNewProject.tsx
+++ src/pages/CreateNewProject/CreateNewProject.tsx
@@ -195,6 +195,7 @@ function CreateNewProject() {
   const [position, setPosition] = useState<{ lat?: number; lng?: number }>({});
   const [title, setTitle] = useState("");
   const [mainImgSrc, setMainImgSrc] = useState("");
+  const [progressing, setProgressing] = useState(100);
   const googleMap = templatesArr[8];
 
   async function confirmAllEdit() {
@@ -263,6 +264,7 @@ function CreateNewProject() {
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgressing(progress);
         },
         (error) => {
           console.log("Upload err", error);
@@ -341,7 +343,17 @@ function CreateNewProject() {
                   multiple
                 />
               </UploadPic>
-              <MainImg img={`url(${mainImgSrc})`} />
+              {progressing === 100 ? (
+                <MainImg img={`url(${mainImgSrc})`} />
+              ) : (
+                <Loading
+                  type="spin"
+                  color="#3c3c3c"
+                  height="40px"
+                  width="40px"
+                />
+              )}
+
               {templateFilter.map(({ keyUuid, ChoseTemplate }, index) => (
                 <SingleEditorContainer key={`${keyUuid}`}>
                   <ChoseTemplate
--- src/pages/PortfolioBricks/PortfolioBricks.tsx
+++ src/pages/PortfolioBricks/PortfolioBricks.tsx
@@ -40,6 +40,7 @@ const Wrapper = styled.div`
   width: 100%;
   min-width: 100vw;
   height: 100%;
+  min-height: calc(100vh - 80px);
   position: relative;
   display: flex;
   flex-direction: column;
